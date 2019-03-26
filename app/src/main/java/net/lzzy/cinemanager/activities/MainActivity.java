52bf079dd88e235eb22e366e6f62e2cd84b334f1
maibo666 <MAIbo255002>
2019-03-26 17:55:02 +0800

    添加影院列表和订单，影院日期

--- app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
+++ app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
@@ -1,19 +1,89 @@
 package net.lzzy.cinemanager.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import net.lzzy.cinemanager.R;
+import net.lzzy.cinemanager.fragments.CinemasFragment;
+import net.lzzy.cinemanager.fragments.OrderFragment;
+
+import java.util.MissingFormatArgumentException;
 
 /**
  * @author Administrator
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private View layoutMenu;
+    private TextView tvTitle;
+    private SearchView search;
+    private FragmentManager manger=getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /** 去掉标题栏 **/
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        setTitleMenu();
+    }
+
+    /** 标题栏 **/
+    private void setTitleMenu() {
+        layoutMenu = findViewById(R.id.bar_title_layout_menu);
+        layoutMenu.setVisibility(View.GONE);
+        findViewById(R.id.bar_title_img_menu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int visible=layoutMenu.getVisibility()==View.VISIBLE ? View.GONE : View.VISIBLE;
+                layoutMenu.setVisibility(visible);
+            }
+        });
+        tvTitle = findViewById(R.id.bar_title_tv_title);
+        tvTitle.setText(R.string.bar_title_menu_orders);
+        search = findViewById(R.id.main_sv_search);
+        findViewById(R.id.bar_title_tv_add_cinema).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_view_cinema).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_add_order).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_view_order).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_exit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        layoutMenu.setVisibility(View.GONE);
+        switch (v.getId()){
+            case R.id.bar_title_tv_add_cinema:
+                break;
+            case R.id.bar_title_tv_view_cinema:
+                tvTitle.setText(R.string.bar_title_menu_cinema);
+                manger.beginTransaction()
+                        .replace(R.id.fragment_container,new CinemasFragment())
+                        .commit();
+                break;
+            case R.id.bar_title_tv_add_order:
+                break;
+            case R.id.bar_title_tv_view_order:
+                tvTitle.setText(R.string.bar_title_menu_orders);
+                manger.beginTransaction().
+                        replace(R.id.fragment_container,new OrderFragment())
+                        .commit();
+                break;
+            default:
+                break;
+
+        }
+
     }
 }
