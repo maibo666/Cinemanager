e2b6b30e466dcb31332ceaf0b93c6caaa9c67b6c
maibo666 <MAIbo255002>
2019-04-01 08:31:43 +0800

    4月1

--- app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
+++ app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
@@ -1,49 +1,55 @@
 package net.lzzy.cinemanager.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-
 import android.os.Bundle;
+import android.util.SparseArray;
 import android.view.View;
 import android.view.Window;
+import android.widget.LinearLayout;
 import android.widget.SearchView;
 import android.widget.TextView;
 
-import net.lzzy.cinemanager.R;
-import net.lzzy.cinemanager.fragments.CinemasFragment;
-import net.lzzy.cinemanager.fragments.OrderFragment;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import java.util.MissingFormatArgumentException;
+import net.lzzy.cinemanager.R;
+import net.lzzy.cinemanager.fragments.AddCinemasFragment;
+import net.lzzy.cinemanager.fragments.AddOrdersFragment;
+import net.lzzy.cinemanager.fragments.CinemasFragment;
+import net.lzzy.cinemanager.fragments.OrdersFragment;
+import net.lzzy.cinemanager.models.Cinema;
 
 /**
  * @author Administrator
  */
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private View layoutMenu;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,
+        AddCinemasFragment.OnFragmentInteractionListener,AddCinemasFragment.OnCinemaCreatedListener{
+    private FragmentManager manager=getSupportFragmentManager();
+    private LinearLayout layoutMenu;
     private TextView tvTitle;
     private SearchView search;
-    private FragmentManager manger=getSupportFragmentManager();
+    private SparseArray<String> titleArray=new SparseArray<>();
+    private SparseArray<Fragment> fragmentArray = new SparseArray<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /** 去掉标题栏 **/
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         setTitleMenu();
     }
 
-    /** 标题栏 **/
     private void setTitleMenu() {
+        titleArray.put(R.id.bar_title_tv_add_cinema,"添加影院");
+        titleArray.put(R.id.bar_title_tv_view_cinema,"影院列表");
+        titleArray.put(R.id.bar_title_tv_add_order,"添加订单");
+        titleArray.put(R.id.bar_title_tv_view_order,"订单列表");
         layoutMenu = findViewById(R.id.bar_title_layout_menu);
         layoutMenu.setVisibility(View.GONE);
-        findViewById(R.id.bar_title_img_menu).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int visible=layoutMenu.getVisibility()==View.VISIBLE ? View.GONE : View.VISIBLE;
-                layoutMenu.setVisibility(visible);
-            }
+        findViewById(R.id.bar_title_img_menu).setOnClickListener(v -> {
+            int visible=layoutMenu.getVisibility()==View.VISIBLE ? View.GONE : View.VISIBLE;
+            layoutMenu.setVisibility(visible);
         });
         tvTitle = findViewById(R.id.bar_title_tv_title);
         tvTitle.setText(R.string.bar_title_menu_orders);
@@ -52,38 +58,79 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.bar_title_tv_view_cinema).setOnClickListener(this);
         findViewById(R.id.bar_title_tv_add_order).setOnClickListener(this);
         findViewById(R.id.bar_title_tv_view_order).setOnClickListener(this);
-        findViewById(R.id.bar_title_tv_exit).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                System.exit(0);
-            }
-        });
+        findViewById(R.id.bar_title_tv_exit).setOnClickListener(v -> System.exit(0));
     }
 
     @Override
     public void onClick(View v) {
+        search.setVisibility(View.VISIBLE);
         layoutMenu.setVisibility(View.GONE);
-        switch (v.getId()){
-            case R.id.bar_title_tv_add_cinema:
-                break;
-            case R.id.bar_title_tv_view_cinema:
-                tvTitle.setText(R.string.bar_title_menu_cinema);
-                manger.beginTransaction()
-                        .replace(R.id.fragment_container,new CinemasFragment())
-                        .commit();
-                break;
-            case R.id.bar_title_tv_add_order:
-                break;
-            case R.id.bar_title_tv_view_order:
-                tvTitle.setText(R.string.bar_title_menu_orders);
-                manger.beginTransaction().
-                        replace(R.id.fragment_container,new OrderFragment())
-                        .commit();
-                break;
-            default:
-                break;
-
+        tvTitle.setText(titleArray.get(v.getId()));
+        FragmentTransaction transaction=manager.beginTransaction();
+        Fragment fragment=fragmentArray.get(v.getId());
+        if (fragment==null){
+            fragment=createFragment(v.getId());
+            fragmentArray.put(v.getId(),fragment);
+            transaction.add(R.id.fragment_container,fragment);
         }
+        for (Fragment f:manager.getFragments()){
+            transaction.hide(f);
+        }
+        transaction.show(fragment).commit();
+    }
 
+
+    private Fragment createFragment(int id) {
+        switch (id) {
+            case R.id.bar_title_tv_add_cinema:
+                return new AddCinemasFragment();
+            case R.id.bar_title_tv_view_cinema:
+                return new CinemasFragment();
+            case R.id.bar_title_tv_add_order:
+                return new AddOrdersFragment();
+            case R.id.bar_title_tv_view_order:
+                return new OrdersFragment();
+            default:
+                return new OrdersFragment();
+        }
+    }
+
+    @Override
+    public void hideSearch() {
+        search.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void cancelAddCinema() {
+        Fragment addCinemaFragment = fragmentArray.get(R.id.bar_title_tv_add_cinema);
+        if(addCinemaFragment == null){
+            return;
+        }
+        Fragment cinemasFragment = fragmentArray.get(R.id.bar_title_tv_view_cinema);
+        FragmentTransaction transaction = manager.beginTransaction();
+        if(cinemasFragment == null){
+            cinemasFragment = new CinemasFragment();
+            fragmentArray.put(R.id.bar_title_tv_view_cinema,cinemasFragment);
+            transaction.add(R.id.fragment_container,cinemasFragment);
+        }
+        transaction.hide(addCinemaFragment).show(cinemasFragment).commit();
+        tvTitle.setText(titleArray.get(R.id.bar_title_tv_add_cinema));
+    }
+
+    @Override
+    public void saveCinema(Cinema cinema) {
+        Fragment addCinemaFragment = fragmentArray.get(R.id.bar_title_tv_add_cinema);
+        if(addCinemaFragment == null){
+            return;
+        }
+        Fragment cinemasFragment = fragmentArray.get(R.id.bar_title_tv_view_cinema);
+        FragmentTransaction transaction = manager.beginTransaction();
+        if(cinemasFragment == null){
+            //创建cinemasFragment同事要传cinema对象进来
+        }else {
+            ((CinemasFragment)cinemasFragment).save(cinema);
+        }
+        transaction.hide(addCinemaFragment).show(cinemasFragment).commit();
+        tvTitle.setText(titleArray.get(R.id.bar_title_tv_view_cinema));
     }
 }
--- app/src/main/java/net/lzzy/cinemanager/fragments/CinemasFragment.java
+++ app/src/main/java/net/lzzy/cinemanager/fragments/CinemasFragment.java
@@ -1,29 +1,63 @@
 package net.lzzy.cinemanager.fragments;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import android.widget.ListView;
 
 import net.lzzy.cinemanager.R;
+import net.lzzy.cinemanager.models.Cinema;
+import net.lzzy.cinemanager.models.CinemaFactory;
+import net.lzzy.sqllib.GenericAdapter;
+import net.lzzy.sqllib.ViewHolder;
 
-/**
- * Created by lzzy_gxy on 2019/3/26.
- * Description:
- */
-public class CinemasFragment extends Fragment {
+import java.util.List;
+
+
+public class CinemasFragment extends BaseFragment {
+
+    private List<Cinema> cinemas;
+    private ListView lv;
+    private CinemaFactory factory= CinemaFactory.getInstance();
+    private GenericAdapter<Cinema> adapter;
+    private Cinema cinema;
+
     public CinemasFragment(){}
-
-    @Nullable
+    public CinemasFragment(Cinema cinema){
+        this.cinema = cinema;
+    }
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view= inflater.inflate(R.layout.fragment_cinemas,null);
-        TextView textView=view.findViewById(R.id.fragment_container);
-        return view;
+    protected void populate() {
+        lv = find(R.id.activity_cinema_lv);
+        View empty=find(R.id.activity_cinemas_tv_none);
+        lv.setEmptyView(empty);
+        cinemas=factory.get();
+        adapter = new GenericAdapter<Cinema>(getActivity(),
+                R.layout.cinemas_item,cinemas) {
+            @Override
+            public void populate(ViewHolder viewHolder, Cinema cinema) {
+                viewHolder.setTextView(R.id.cinemas_items_name,cinema.getName())
+                        .setTextView(R.id.cinemas_items_location,cinema.getLocation());
+            }
+
+            @Override
+            public boolean persistInsert(Cinema cinema) {
+                return factory.addCinema(cinema);
+            }
+
+            @Override
+            public boolean persistDelete(Cinema cinema) {
+                return factory.deleteCinema(cinema);
+            }
+        };
+        lv.setAdapter(adapter);
+    }
+
+    public void save(Cinema cinema){
+        adapter.add(cinema);
+    }
+
+
+    @Override
+    public int getLayoutRes() {
+        return R.layout.fragment_cinemas;
     }
 }
